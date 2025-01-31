64d6e35ab46aa841242df4aae99d4936cb03e4fd
PedroDeVvV <pedrohgs2004@gmail.com>
2025-01-31 12:06:00 -0300

    FEAT: restricting endpoints with JWT auth0

--- api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,25 +1,31 @@
 package med.voll.api.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
 
 @Service
 public class TokenService {
 
+    @Value("${api.security.token.secret}")
+    private String secret;
 
     public String gerarToken(Usuario usuario) {
-        try {
-            var algoritmo = Algorithm.HMAC256("1234578");
 
-            return JWT.create().withIssuer("API Voll.med") //identifica a api
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withIssuer("API Voll.med") //identifica a api
                     .withSubject(usuario.getLogin())
                     .withExpiresAt(dataExpiracao())
                     .withClaim("id", usuario.getId()) //identifica o usuário
@@ -28,6 +34,19 @@ public class TokenService {
         } catch (JWTCreationException exception) {
             // Invalid Signing configuration / Couldn't convert Claims.
             throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
